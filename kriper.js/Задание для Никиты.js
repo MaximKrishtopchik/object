b21fd4427f18ecc84cec2838026b9cc99f9bf3c1
Maxim <119208216+MaximKrishtopcik@users.noreply.github.com>
2023-02-21 23:00:55 +0300

    https://github.com/MaximKrishtopchik/object.git

---
+++ kriper.js/Задание для Никиты.js
@@ -0,0 +1,197 @@
+//1
+//Напишите цикл который выводит все число о 1 до 100 вкдючительно.  и выводит в консоль
+//Решение
+//          for(let i = 1; i<=100; i++){
+//             console.log(i)
+//           } 
+
+
+
+//2
+//Напишите цикл который выводит все числа от 100 до 1 вкдючительною и выводит в консоль
+//Решенине
+//          for (let i = 100; i>0; i--){
+//              console.log(i)
+//          }
+
+
+
+//3
+//Выведите в столбец все четные числа в промежутке от 1 до 100 используе continue. и вывести это в консоль
+//Решение
+//          for(let i = 1; i< 100; i++){
+//              if(i % 2 == 1) continue
+//              console.log(i)
+//              }
+
+
+
+//4
+//Выведите в столбец все нечетные числа в промежутке от 1 до 100 используе continue. и вывести это в консоль
+//Решение
+//          for(let i = 1; i< 100; i++){
+//              if(i % 2 == 0) continue
+//              console.log(i)
+//             }
+
+
+
+//5
+//Дан массив с элементами [2, 4, 4, 6]. С помощью цикла for найдите сумму элементов этого массива
+//Решение
+//          let result = 1;
+//          let arr = [2, 3, 4, 5];
+//          for (let i = 0; i < arr.length; i++) {
+//	          result = result + arr[i];
+//          }
+//         alert(result);
+
+
+                                    
+//6
+//Дан массив с элементами [2, 4, 4, 6]. С помощью цикла for найдите произведение элементов этого массива
+//Решение
+//          let result = 1;
+//         let arr = [2, 4, 4, 6];
+//         for (let i = 0; i < arr.length; i++) {
+//             	result = result * arr[i];
+//          }
+//          alert(result);
+
+
+
+
+//1
+// let arr = [1,2,3,4,5]
+// for(let i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+
+
+//2
+// let result = 0;
+// let arr = [2,3,4,5];
+// for(let i = 0; i < arr.length; i++){
+//     result = result + arr[i];
+// } console.log(result)
+
+
+//3
+//  let result = 0;
+//  for(let i = 1; i <= 100; i++){
+//      result = result  + i;
+//  } console.log(result)
+
+
+//4
+
+
+// let arr = [1,2,3,4,5]
+// for(let i = 0; i < arr.length; i++){
+//          console.log(arr[i])
+// }
+
+//  let result = 0;
+//  let arr = [2,3,4,5];
+//  for(let i = 0; i < arr.length; i++){
+//      result = result + arr[i];
+//  } console.log(result)
+// let arr = [2,5,9,15,0,4];
+// for(i = 0; i <arr.length; i++){
+//     if (arr[i] > 3 && arr[i] <
+//     10){
+
+// console.log(arr[i])        
+//     }
+// }
+
+   
+
+// let arr = [2,3,4,9,0,34];
+// for (let i = 0; i< arr.length; i++){
+//     if (arr[i] > 3 && arr[i] < 10){
+//         console.log(arr[i])
+//     }
+// }
+
+
+
+
+// let arr = ['понедельник' , 'вторник', 'средвба', 'цетверг', '2'];
+// let day = ['2'];
+// for( let i = 0; i< arr.length; i++){
+//     if(arr == 2) continue
+// } console.log(arr[i])
+
+
+// for(let i = 1; i< 100; i++){
+//     if(i % 2 == 1) continue
+//    console.log(i)
+// }
+
+
+
+
+
+// for (let i = 1; i < 100; i++){
+//     if (i % 2 == 0) continue
+//     console.log(i)
+// } 
+
+// let num;
+// do{
+// num = prompt("Введите число больше 100")
+// } while( num <=100 && num)
+
+
+// for (let i = 1; i>=100; i--){
+//     console.log(i)
+// }
+
+//Функции
+// function num1(a,b){
+//     return a + b
+// } 
+// let result = num1(1,4);
+// console.log(result)
+
+
+// function num (a, b){
+//     return a + b
+// } 
+// let result = num(1,2)
+// console.log(result)
+
+
+
+
+
+
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('А родители разрешили?');
+//   }
+// }
+
+// let age = prompt('Сколько вам лет?', 18);
+
+// if ( checkAge(age) ) {
+//   alert( 'Доступ получен' );
+// } else {
+//   alert( 'Доступ закрыт' );
+// }
+
+// function min(a,b){ 
+//     if(a < b){
+//         return a
+//     }   else{
+//             return b
+//         }
+//     }
+
+
+
